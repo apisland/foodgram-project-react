58f30f283577e4421664180be47027b0d339957a
Валентин Климов <dd_apis@mail.ru>
2022-10-19 02:03:14 +0300

    foodgram_v1.6

--- backend/foodgram/api/permissions.py
+++ backend/foodgram/api/permissions.py
@@ -3,6 +3,7 @@ from rest_framework import permissions
 
 class IsAdminOrAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS
-                or obj.author == request.user
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return (obj.author == request.user
                 or request.user.is_staff)
