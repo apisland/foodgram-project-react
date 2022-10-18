a7802c673409de85395f57d36f8e96184133a0e0
Валентин Климов <dd_apis@mail.ru>
2022-10-19 02:26:07 +0300

    foodgram_v1.6

--- backend/foodgram/api/permissions.py
+++ backend/foodgram/api/permissions.py
@@ -6,4 +6,4 @@ class IsAdminOrAuthorOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return (obj.author == request.user
-                or request.user.is_staff)
+                or request.user.is_admin)
